d9631e792c26e379a2b88a4d9ae370e6bd73232c
longnd97 <nduylong060697@gmail.com>
2021-11-01 22:49:42 +0700

    register-logic-frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './pages/login/login.component';
 import {HttpClientModule} from "@angular/common/http";
-import { HomeComponent } from './pages/home/home.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { NavbarComponent } from './layouts/navbar/navbar.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { MasterComponent } from './layouts/master/master.component';
-import { RegisterComponent } from './pages/register/register.component';
+import {HomeComponent} from './pages/home/home.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NavbarComponent} from './layouts/navbar/navbar.component';
+import {FooterComponent} from './layouts/footer/footer.component';
+import {MasterComponent} from './layouts/master/master.component';
+import {RegisterComponent} from './pages/register/register.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
@@ -25,10 +27,12 @@ import { RegisterComponent } from './pages/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

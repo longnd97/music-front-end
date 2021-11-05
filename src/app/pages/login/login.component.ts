b5d13f767c66e781c01e1d8fa90e9b26fc5d7b65
khonghung <ahihimaianh3@gmail.com>
2021-11-05 10:55:55 +0700

    pull-list-song

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -29,6 +29,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    window.scroll(0,250);
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required],

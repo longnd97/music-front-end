7ebdac123a05403135950d1a1bfc6180cba439f0
longnd97 <nduylong060697@gmail.com>
2021-11-08 17:13:10 +0700

    add song to playlist

---
+++ src/app/playlistManage/my-playlist/my-playlist.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {PlaylistService} from "../../services/playlist.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-my-playlist',
+  templateUrl: './my-playlist.component.html',
+  styleUrls: ['./my-playlist.component.css']
+})
+export class MyPlaylistComponent implements OnInit {
+  user_id: any;
+  myPlaylists: any;
+  playlist: any;
+
+  constructor(private playlistService: PlaylistService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.user_id = localStorage.getItem('id');
+    this.getMyPlaylists();
+  }
+
+  getMyPlaylists() {
+    this.playlistService.getMyPlaylists(this.user_id).subscribe(res => {
+      this.myPlaylists = res;
+    })
+  }
+
+  getPlaylist(id: number) {
+    this.router.navigate(['playlist/' + id + '/detail']);
+  }
+}
+

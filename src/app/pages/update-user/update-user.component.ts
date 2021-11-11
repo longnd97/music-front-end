82f882b547fcc43dd980046fa9c061ef46b9abc1
longnd97 <nduylong060697@gmail.com>
2021-11-11 08:34:23 +0700

    test lan 1

--- src/app/pages/update-user/update-user.component.ts
+++ src/app/pages/update-user/update-user.component.ts
@@ -3,7 +3,6 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../services/user.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {User} from "../../interfaces/user";
-import {first} from "rxjs/operators";
 import {AngularFireStorage, AngularFireStorageReference} from "@angular/fire/storage";
 
 @Component({
@@ -48,7 +47,6 @@ export class UpdateUserComponent implements OnInit {
     this.userService.getInfoUserLogin().subscribe((res: User) => {
       this.user = res;
       this.avatar=res.avatar;
-      console.log(this.user);
       this.formUpdate?.setValue({
         full_name: res.full_name,
         phone: res.phone,
@@ -65,7 +63,6 @@ export class UpdateUserComponent implements OnInit {
       // @ts-ignore
      /* this.formUpdate.controls.avatar.setValue(this.avatar);*/
       let data = this.formUpdate?.value;
-      console.log(data);
       this.userService.updateById(id, data).subscribe(
         res => {
           this.router.navigate(['users']).then(() => {
--- src/app/songManage/many-liked-list-song/many-liked-list-song.component.ts
+++ src/app/songManage/many-liked-list-song/many-liked-list-song.component.ts
@@ -25,50 +25,10 @@ export class ManyLikedListSongComponent implements OnInit {
   getSongsManyLikedSongs() {
     this.songService.getSongManyLiked().subscribe(res => {
       this.manyLikedSongs = res;
-      for (let song of res) {
-        if (song.status == 1) {
-          // @ts-ignore
-          this.color[song.id] = 'primary';
-          // @ts-ignore
-          this.icon[song.id] = 'thumb_up';
-          // @ts-ignore
-          this.message[song.id] = 'Đã thích';
-        } else {
-          // @ts-ignore
-          this.color[song.id] = '';
-          // @ts-ignore
-          this.icon[song.id] = 'thumb_up_off_alt';
-          // @ts-ignore
-          this.message[song.id] = 'Thích';
-        }
-      }
     });
   }
-
   playSong(event: any, songId: number) {
     event.preventDefault();
     this.songId.emit(songId);
-  }
-
-  Liked(songId: number) {
-    let data = {
-      'user_id': this.user_id,
-      'song_id': songId
-    }
-    this.songService.Liked(data).subscribe(res => {
-      if (res.status === 'liked') {
-        // @ts-ignore
-        this.color[songId] = 'primary';
-        // @ts-ignore
-        this.icon[songId] = 'thumb_up';
-        this.message[songId] = 'Đã thích';
-      } else {
-        // @ts-ignore
-        this.color[songId] = '';
-        // @ts-ignore
-        this.icon[songId] = 'thumb_up_off_alt';
-        this.message[songId] = 'Thích';
-      }
-    });
   }
 }

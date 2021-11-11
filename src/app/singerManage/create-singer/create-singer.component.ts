9a1eef046f49887b9553c0ad8643a1c470b17766
nguyentuyen348 <nguyentuyen348@gmail.com>
2021-11-11 10:19:03 +0700

    create singer

--- src/app/singerManage/create-singer/create-singer.component.ts
+++ src/app/singerManage/create-singer/create-singer.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 import {SingerService} from "../../services/singer.service";
+import {ERROR} from "@angular/compiler-cli/src/ngtsc/logging/src/console_logger";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-create-singer',
@@ -21,11 +23,11 @@ export class CreateSingerComponent implements OnInit {
     this.createSingerForm = this.fb.group({
       name: ['', [Validators.required]],
       gender: ['', [Validators.required]],
-      date_of_birth: [''],
-      band: [''],
-      information: ['', []],
-      story: ['', []],
-      avatar: ['', []],
+      date_of_birth: ['',[Validators.required]],
+      band: ['',[Validators.required]],
+      information: ['', [Validators.required]],
+      story: ['', [Validators.required]],
+      avatar: [''],
 
     });
   }
@@ -42,8 +44,10 @@ export class CreateSingerComponent implements OnInit {
       if (res.status === 'success') {
         alert(res.message);
         window.location.reload();
+      } else
+      {
+        alert(res.message);
       }
-        alert('Thêm ca sĩ thất bại !');
     });
   }
   uploadAvatar(event: string) {
@@ -52,25 +56,25 @@ export class CreateSingerComponent implements OnInit {
   selectedGender(event: any) {
     this.selected = event.target.value;
   }
+
+
   get name() {
-    // @ts-ignore
-    return this.createSingerForm.get('name');
+    return this.createSingerForm?.get('name');
   }
   get gender() {
-    // @ts-ignore
-    return this.createSingerForm.get('gender');
+    return this.createSingerForm?.get('gender');
   }
   get date_of_birth() {
-    // @ts-ignore
-    return this.createSingerForm.get('date_of_birth');
+    return this.createSingerForm?.get('date_of_birth');
   }
   get band() {
-    // @ts-ignore
-    return this.createSingerForm.get('band');
+    return this.createSingerForm?.get('band');
   }
   get information() {
-    // @ts-ignore
-    return this.createSingerForm.get('information');
+    return this.createSingerForm?.get('information');
+  }
+  get story() {
+    return this.createSingerForm?.get('story');
   }
 
 }
